845e72f27d18c31b75be15ed11e342291e3bbdcf
Hoyoung Jung <honux77@gmail.com>
2022-10-05 00:38:24 +0900

    blink 프로젝트 추가

---
+++ day1-setup/blink/blink.v
@@ -0,0 +1,27 @@
+module blink (
+    input wire FPGA_CLK1_50, //50mhz input clock
+    output wire LED_POWER // LED output    
+);
+
+wire clk;
+
+clk_pll (
+		.refclk(FPGA_CLK1_50),   //  refclk.clk
+		.rst(),      //   reset.reset
+		.outclk_0(clk)  // outclk0.clk
+	);
+
+
+reg [31:0] cnt; //32-bit counter
+
+initial begin
+    cnt <= 32'h00000000;
+end
+
+always @(posedge clk) begin
+    cnt <= cnt + 1;
+end
+
+assign LED_POWER = cnt[24];
+
+endmodule
